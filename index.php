9120692a2f3f0330ccfc44f05338d1197cd546cc
you <anusorn1039@gmail.com>
2023-02-04 01:18:36 +0700

    (1.17am Add f webservice)_

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>UMI+STARLEADER</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="./assets-render/main.css">
+</head>
+
+<body>
+  <nav>
+    <div class="umi-container">
+      <ul class="umi-rows umi-justify-content-between">
+        <ul class="umi-rows umi-justify-content-center">
+          <li class="mg-1">home</li>
+          <li class="mg-1">home</li>
+          <li class="mg-1">home</li>
+        </ul>
+        <ul class="umi-rows umi-justify-content-center">
+          <li class="mg-1">home</li>
+          <li class="mg-1">home</li>
+          <li class="mg-1">home</li>
+        </ul>
+      </ul>
+
+    </div>
+  </nav>
+  <script src="./roots/test.js"></script>
+</body>
+
+</html>
---
+++ src-types/test.ts
@@ -0,0 +1,6 @@
+const userName:string = "UMI" ; 
+let element = document.getElementById("hellow") as HTMLElement
+
+element.innerHTML = "Hello"
+
+

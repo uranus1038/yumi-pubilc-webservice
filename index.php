91da639614d927b8db07c9c3021dfd1df501dd87
you <anusorn1039@gmail.com>
2023-02-14 03:39:47 +0700

    Add navbarWeb

--- index.php
+++ index.php
@@ -6,29 +6,42 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>UMI+STARLEADER</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <link rel="stylesheet" href="./assets-render/main.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./assets-render/god-main.css" />
+  <link rel="stylesheet" href="./assets-render/navbar/navbar.css" />
 </head>
 
 <body>
   <nav>
-    <div class="umi-container">
-      <ul class="umi-rows umi-justify-content-between">
-        <ul class="umi-rows umi-justify-content-center">
-          <li class="mg-1">home</li>
-          <li class="mg-1">home</li>
-          <li class="mg-1">home</li>
+    <ul class=" umi-container-full">
+      <div class="umi-navbar umi-special-blue-c0">
+        <ul class="umi-nav ">
+          <div>
+            <a href="#" class="umi-col f-shiro">Brand</a>
+          </div>
+          <li><a href="#" class="umi-col f-shiro umi-active">Get started</a></li>
+          <li><a href="#" class="umi-col f-shiro umi-active">Doc</a></li>
+          <li>
+            <a href="#" class="umi-col umi-dropdown f-shiro umi-active">Services <i class="fas fa-sort-down"></i></a>
+            <div class="umi-dropdown-content umi-special-blue-c1">
+              <a href="#" class="umi-dropdown-link f-shiro umi-active">UMIStyle</a>
+              <a href="#" class="umi-dropdown-link f-shiro umi-active">UMIFramwork.net</a>
+            </div>
+          </li>
         </ul>
-        <ul class="umi-rows umi-justify-content-center">
-          <li class="mg-1">home</li>
-          <li class="mg-1">home</li>
-          <li class="mg-1">home</li>
+        <ul class="umi-nav ">
+          <li><a href="#" class="umi-col f-shiro umi-active">home</a>
+        </li>
         </ul>
-      </ul>
-
+    </ul>
     </div>
   </nav>
-  <script src="./roots/test.js"></script>
+  <section>
+
+  </section>
 </body>
 
 </html>

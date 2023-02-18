836093e2531de288ddd3c2d0bdca3069765f2dd4
you <anusorn1039@gmail.com>
2023-02-19 02:15:40 +0700

    add page

--- index.php
+++ index.php
@@ -10,34 +10,34 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="./assets-render/god-main.css" >
-  <link rel="stylesheet" href="./assets-render/navbar/navbar.css" >
+  <link rel="stylesheet" href="./assets-render/god-main.css">
+  <link rel="stylesheet" href="./assets-render/navbar/navbar.css">
 </head>
 
 <body class="umi-special-blue-bg">
   <nav>
-    <ul class=" umi-container-full">
+    <div class=" umi-container-full">
       <div class="umi-navbar umi-special-blue-c0">
         <ul class="umi-nav ">
           <div>
-            <a href="#" class="umi-col f-shiro">Brand</a>
+            <a href="#" class="umi-nav-brand f-shiro">Brand</a>
           </div>
-          <li><a href="#" class="umi-col f-shiro umi-active">Get started</a></li>
-          <li><a href="#" class="umi-col f-shiro umi-active">Doc</a></li>
-          <li>
-            <a href="#" class="umi-col umi-dropdown f-shiro umi-active">Services <i class="fas fa-sort-down"></i></a>
+          <li><a href="#" class="umi-nav-item f-shiro umi-active">Get started</a></li>
+          <!-- <li>
+            <a href="#" class="umi-nav-item umi-dropdown f-shiro umi-active">Services <i class="fas fa-sort-down"></i></a>
             <div class="umi-dropdown-content umi-special-blue-c1">
-              <a href="./UMIStyle.php" class="umi-dropdown-link f-shiro umi-active">UMIStyle</a>
+              <a href="./UMIStyle.php" class="umi-dropdown-link f-shiro umi-active">UMICreator.css</a>
               <a href="#" class="umi-dropdown-link f-shiro umi-active">UMIFramwork.net</a>
             </div>
-          </li>
+          </li> -->
         </ul>
         <ul class="umi-nav ">
-        <li><button href="#" class="umi-col btn umi-btn-outness-shiro umi-btn-square f-shiro umi-active fs-medium ">
-            <i class="fas fa-globe"></i> TH</button>
-        </li>
+          <li>
+            <div href="#" class="umi-nav-item umi-border-shiro f-shiro fs-medium ">
+              <i class="fas fa-globe"></i> ENG</ฟ>
+          </li>
         </ul>
-    </ul>
+      </div>
     </div>
   </nav>
   <section>
